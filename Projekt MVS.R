a1dbe8b164df6a8367556cdb078adfc10e1ccc54
Draglus <74567577+Draglus@users.noreply.github.com>
2024-05-14 11:24:59 +0200

    Update Projekt MVS.R

--- Projekt MVS.R
+++ Projekt MVS.R
@@ -2,11 +2,19 @@ library(tidyverse)
 library(ggpubr)
 library(MASS) 
 library(tidyverse) 
+library(rstatix)
 library(biotools)
 library(caret) 
 library(nnet)
-library(rstatix)
 library(pROC)
+library(ggplot2)
+
+library(corrr)
+library(ggcorrplot)
+library("FactoMineR")
+
+
+
 dfc <- read.csv("C:/Users/Mathi/Documents/Studie/2 semester/ST514, DS805 Multivariat Statistisk Analyse/Projekt/winequality-red.csv",header = TRUE)
 
 summary(dfc)
@@ -46,8 +54,8 @@ print(shapiro_p_table)
 
 
 # We decide that quality can be put into three groups Bad, Fine and Great.
-dfc$grade <- case_when(dfc$quality <= 7 ~ "Not great",
-                       dfc$quality > 7 ~ "Great")
+dfc$grade <- case_when(dfc$quality <= 6 ~ "Not great",
+                       dfc$quality > 6 ~ "Great")
 
 
 
@@ -133,6 +141,15 @@ dfc$grade <- case_when(dfc$quality <= 4 ~ "bad",
                        dfc$quality > 6 ~ "Great")
 dfc <- dfc[,-12]
 box_m(dfc[,1:11],dfc[,"grade"])
+numeric_cols <- sapply(dfc, is.numeric)  
+min_max_scale <- function(x) {
+  (x - min(x, na.rm = TRUE)) / (max(x, na.rm = TRUE) - min(x, na.rm = TRUE))
+}
+
+# Apply min-max scaling to numeric columns
+dfc[numeric_cols] <- lapply(dfc[numeric_cols], min_max_scale)
+
+
 #We can see that there are atleast two variables, that still do not have the same covariance matrices
 
 
@@ -177,3 +194,64 @@ cat("Accuracy:", accuracy)
 # 
 #𝐸(APER)
 aer(test.data$grade,test.data$predicted)
+
+
+
+
+
+
+#analysis of how well the models did
+
+
+predictions <- predict(model, newdata = dfc, type = "class")
+
+dfc$predicted <-predictions 
+dfc$predicted <- as.factor(dfc$predicted)
+dfc$grade <- as.factor(dfc$grade)
+# Create a confusion matrix for predicted vs. grade
+conf_matrix <- confusionMatrix(dfc$predicted, dfc$grade)
+
+conf_matrix[["byClass"]]
+
+
+#PCA
+
+
+
+
+#We choose only the numerical data and scale it.
+
+numerical_data <- dfc[,1:11]
+data_normalized <- scale(numerical_data)
+
+#After that we create a correlation matrix
+corr_matrix <- cor(data_normalized)
+ggcorrplot(corr_matrix)
+
+#Now we make a pca
+data.pca <- princomp(corr_matrix)
+summary(data.pca)
+data.pca$loadings[, 1:2]
+
+
+pca_1_2 <-data.pca$loadings[, 1:2]
+pca_1_2<-as.matrix(pca_1_2)
+numerical_data<-as.matrix(numerical_data)
+
+#we multiply the numerical_data with our first and second pricipal components 
+
+reduced_data<-numerical_data %*%pca_1_2
+reduced_data<-as.data.frame(reduced_data)
+reduced_data$predicted <- dfc$predicted
+reduced_data$true_class <- dfc$grade
+
+
+library(gridExtra)
+
+plot1 <- ggplot(reduced_data, aes(x = Comp.1, y = Comp.2, colour = true_class)) +
+  geom_point()
+
+plot2 <- ggplot(reduced_data, aes(x = Comp.1, y = Comp.2, colour = predicted)) +
+  geom_point()
+
+grid.arrange(plot1, plot2, ncol = 2)
